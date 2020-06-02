53098fd66b224e38aa7dce3a5073dc8e6eada3b0
Multezem Kedir <multekedir@users.noreply.github.com>
2020-06-02 08:46:51 -0700

    moved url to environment.ts

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,28 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import { Game } from '../models/game';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Game} from '../models/game';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor(private urlService: UrlService, private http: HttpClient) { }
+  constructor(private urlService: UrlService, private http: HttpClient) {
+  }
 
   addGame(game: Game): Observable<Game> {
-    return this.http.post(this.urlService.getUrl() + 'game', game, {withCredentials: true}).pipe(
-      map( resp => resp as Game)
+    return this.http.post(`${environment.appUrl}/${game}`, {withCredentials: true}).pipe(
+      map(resp => resp as Game)
     );
   }
 
-  updateCat(game: Game): Observable<object> {
-    return this.http.put(this.urlService.getUrl() + 'game/' + game.id, game, {withCredentials: true}).pipe();
-  }
-
-  deleteCat(game: Game): Observable<object> {
-    return this.http.delete(this.urlService.getUrl() + 'game/' + game.id).pipe();
-  }
+  //
+  // updateCat(game: Game): Observable<object> {
+  //   return this.http.put(this.urlService.getUrl() + 'game/' + game.id, game, {withCredentials: true}).pipe();
+  // }
+  //
+  // deleteCat(game: Game): Observable<object> {
+  //   return this.http.delete(this.urlService.getUrl() + 'game/' + game.id).pipe();
+  // }
 }
