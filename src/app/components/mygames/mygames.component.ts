bd2b9553f09c6afe7d52bdfa5ae757625b412c2f
Brandon White <brandonwhite107@gmail.com>
2020-06-02 13:30:48 -0400

    Adding routing for mygames

--- src/app/components/mygames/mygames.component.ts
+++ src/app/components/mygames/mygames.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-mygames',
   templateUrl: './mygames.component.html',
   styleUrls: ['./mygames.component.css']
 })
+
 export class MygamesComponent implements OnInit {
+  loggedUser: User;
+  myGames: User["games"];
 
-  constructor() { }
+  constructor(private authentService: AuthenticationService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.authentService.login(null, null).subscribe(
+      resp => {
+        this.loggedUser = resp;
+      }
+    );
   }
-
+  
 }
