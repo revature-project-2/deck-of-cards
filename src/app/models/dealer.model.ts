2600ac41889fd21be5cf37f1ea86e8a9bef34b27
Yoshi Baker <tsuyoshibaker@gmail.com>
2020-06-03 12:38:54 -0700

    added model and updtated user model

---
+++ src/app/models/dealer.model.ts
@@ -0,0 +1,9 @@
+export class Dealer {
+
+    handValue:number;
+    bust:boolean;
+    blackJack:boolean;
+    naturalBlackJack:boolean;
+    //need an import of the card object/component
+    //card:cards[];
+}
